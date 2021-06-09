0cde141362c0149e83bb2f0371a9ae352e712142
Rafael <rafael.arenas352@gmail.com>
2021-06-09 15:32:24 -0300

    Example Title component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <Title>Hello world!</Title>
     </div>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Title = ({ children }: any) => {
+  return (
+    <h1
+      style={{
+        color: "#434275",
+      }}
+    >
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
